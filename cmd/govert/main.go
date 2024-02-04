f8fc35ed3c750b8085204d4addbff142c7e1b702
abdealijaroli <abdealiburu03@gmail.com>
2024-02-04 12:15:02 +0530

    Refactor main function and add CLI functionality

--- cmd/govert/main.go
+++ cmd/govert/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/abdealijaroli/govert/pkg/converter"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	err := converter.ConvertMarkdownToHTML("test/input.md", "test/output.html")
-	if err != nil {
-		log.Fatal(err)
-	}
+var rootCmd = &cobra.Command{
+	Use:   "govert",
+	Short: "A CLI for converting Markdown to HTML",
+	Long:  `A Command Line Interface application for converting Markdown to HTML`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var inputFile, outputFile string
 
-	fmt.Printf("File %s converted to %s\n", "test/input.md", "test/output.html")
+		if len(args) < 1 {
+			log.Fatalf("Not enough arguments: expected input and output file paths")
+		} else if len(args) == 1 {
+			inputFile = args[0]
+			outputFile = "output.html"
+		} else {
+			inputFile = args[0]
+			outputFile = args[1]
+		}
+
+		err := converter.ConvertMarkdownToHTML(inputFile, outputFile)
+		if err != nil {
+			log.Fatalf("Error converting Markdown to HTML: %v", err)
+		}
+
+		log.Printf("File %s converted to %s\n", inputFile, outputFile)
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("Error executing command: %v", err)
+	}
+}
+
+func main() {
+	Execute()
 }
